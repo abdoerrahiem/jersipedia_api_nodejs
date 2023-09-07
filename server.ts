7b963e391650bba3a27696d6df9a153ce54fb3b4
Abdur Rahim <abdoerrahiem@gmail.com>
2023-09-07 18:18:28 +0700

    fix build folder

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) =>
-  res.json({ success: true, message: 'Welcome, JersiPedia API' })
+  res.json({ success: true, message: 'JersiPedia API' })
 )
 
 app.use('/api/user', userRoute)
