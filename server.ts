1966ef7a11d138f542e3d6537456e667189cbe45
Abdur Rahim <abdoerrahiem@gmail.com>
2023-09-07 17:53:26 +0700

    update server.ts

--- server.ts
+++ server.ts
@@ -1,27 +1,27 @@
-import express, { Application, Request, Response } from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db";
-import { notFound, errorHandler } from "./middlewares/error";
-import cors from "cors";
-import userRoute from "./routes/userRoute";
+import express, { Application, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db'
+import { notFound, errorHandler } from './middlewares/error'
+import cors from 'cors'
+import userRoute from './routes/userRoute'
 
-const app: Application = express();
+const app: Application = express()
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req: Request, res: Response) =>
-  res.json({ success: true, message: "JersiPedia API" })
-);
+app.get('/', (req: Request, res: Response) =>
+  res.json({ success: true, message: 'Welcome, JersiPedia API' })
+)
 
-app.use("/api/user", userRoute);
+app.use('/api/user', userRoute)
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
+const PORT = process.env.PORT || 8000
+app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`))
